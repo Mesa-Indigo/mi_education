660a8ed5f04a8e12fda4aa8bd8c80c1b2cfcd148
Agimir <william@mesaindigostudios.com>
2024-06-22 18:03:22 -0500

    stuff

---
+++ sql/insert.sql
@@ -0,0 +1,13 @@
+-- Insert these into your DB. Feel free to create more if you want to:
+--      ('cert_name', 'Certification of OogaBooga'),
+--      ('aadg_name', 'Associates Degree in OogaBooga'),
+--      ('bcdg_name', 'Bachelors Degree in OogaBooga'),
+--      ('msdg_name', 'Masters Degree in OogaBooga'),
+--      ('doct_name', 'Doctorial Degree in OogaBooga'),
+
+INSERT INTO `ox_licenses` (`name`, `label`) VALUES
+  ('cert_autom', 'Automotive Tech Certification'),
+  ('cert_boatm', 'Maritime Tech Certification'),
+  ('cert_airfm', 'Airframe Tech Certification'),
+  ('cert_elect', 'Electrician Tech Certification'),
+  ('cert_medic', 'Medical Tech Certification'),
