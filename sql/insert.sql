cac226a3f9c29bb1071ae18c4833c2dff9348bbf
Agimir <william@mesaindigostudios.com>
2024-06-23 18:31:53 -0500

    adjusted sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,13 +1,32 @@
 -- Insert these into your DB. Feel free to create more if you want to:
 --      ('cert_name', 'Certification of OogaBooga'),
+--      ('scrt_name', 'Certification of OogaBooga'),
 --      ('aadg_name', 'Associates Degree in OogaBooga'),
 --      ('bcdg_name', 'Bachelors Degree in OogaBooga'),
 --      ('msdg_name', 'Masters Degree in OogaBooga'),
 --      ('doct_name', 'Doctorial Degree in OogaBooga'),
 
 INSERT INTO `ox_licenses` (`name`, `label`) VALUES
-  ('cert_autom', 'Automotive Tech Certification'),
-  ('cert_boatm', 'Maritime Tech Certification'),
-  ('cert_airfm', 'Airframe Tech Certification'),
-  ('cert_elect', 'Electrician Tech Certification'),
-  ('cert_medic', 'Medical Tech Certification'),
+  -- General Certifications
+  ('cert_const', 'Gen. Construction Cert.'),
+  ('cert_autom', 'Automotive Tech Cert.'),
+  ('cert_boatm', 'Maritime Tech Cert.'),
+  ('cert_airfm', 'Airframe Tech Cert.'),
+  ('cert_elect', 'Electrician Tech Cert.'),
+  ('cert_medic', 'Medical Tech Cert.'),
+  ('cert_pvtsc', 'Private Security Cert.'),
+
+  -- Special Certifications
+  ('scrt_name', 'Private Security Cert.'),
+  
+  -- Associates Degrees
+  ('aadg_name', 'Private Security Cert.'),
+  
+  -- Bachelors Degrees
+  ('bcdg_name', 'Private Security Cert.'),
+  
+  -- Masters Degrees
+  ('msdg_name', 'Private Security Cert.'),
+  
+  -- Doctorial Degreess
+  ('doct_name', 'Private Security Cert.'),
