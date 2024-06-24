59167c5118659e28eebc1dd03d549f8749af36ce
Agimir <william@mesaindigostudios.com>
2024-06-24 06:44:57 -0500

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -8,25 +8,57 @@
 
 INSERT INTO `ox_licenses` (`name`, `label`) VALUES
   -- General Certifications
-  ('cert_const', 'Gen. Construction Cert.'),
-  ('cert_autom', 'Automotive Tech Cert.'),
-  ('cert_boatm', 'Maritime Tech Cert.'),
-  ('cert_airfm', 'Airframe Tech Cert.'),
-  ('cert_elect', 'Electrician Tech Cert.'),
-  ('cert_medic', 'Medical Tech Cert.'),
-  ('cert_pvtsc', 'Private Security Cert.'),
+  ('cert_const', 'Cert: Gen. Construction'),
+  ('cert_autom', 'Cert:'),
+  ('cert_boatm', 'Cert:'),
+  ('cert_airfm', 'Cert:'),
+  ('cert_elect', 'Cert:'),
+  ('cert_medic', 'Cert:'),
+  ('cert_pvtsc', 'Cert:'),
 
-  -- Special Certifications
-  ('scrt_name', 'Private Security Cert.'),
+  -- Special Certifications - Fire / Police / Ems / Mil
+  ('scrt_name', 'Cert:'),
   
-  -- Associates Degrees
-  ('aadg_name', 'Private Security Cert.'),
+  -- Associates Degrees - aadg_name
+  ('aadg_aarts', 'AA in General Arts'),
+  ('aadg_bsadm', 'AA in Business Admin'),
+  ('aadg_crmjs', 'AA in Criminal Justice'),
+  ('aadg_comps', 'AA in Computer Science'),
+  ('aadg_nurse', 'AA in Nursing'),
+  ('aadg_engrs', 'AA in Engineering'),
+  ('aadg_biogl', 'AA in Biology'),
+  ('aadg_chems', 'AA in Chemistry'),
+  ('aadg_journ', 'AA in Journalism'),
   
-  -- Bachelors Degrees
-  ('bcdg_name', 'Private Security Cert.'),
+  -- Bachelors Degrees - bcdg_name
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
+  ('bcdg_name', 'BA in '),
   
-  -- Masters Degrees
-  ('msdg_name', 'Private Security Cert.'),
+  -- Masters Degrees - msdg_name
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
+  ('msdg_name', 'MA in '),
   
-  -- Doctorial Degreess
-  ('doct_name', 'Private Security Cert.'),
+  -- Doctorial Degreess - doct_name
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
+  ('doct_name', 'Ph.D in '),
