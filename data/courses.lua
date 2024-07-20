56d21b9657490108d4593a85666333d0d47add47
Agimir <william@mesaindigostudios.com>
2024-07-20 10:11:22 -0500

    Added art / business courses

--- data/courses.lua
+++ data/courses.lua
@@ -1,5 +1,5 @@
 return {
-
+    ----------------------------------------------------------------
     -- certification courses
     ['crt_01'] = { subject = 'Certification', course = 'General Construction.',     id = 'CRT001' },
     ['crt_02'] = { subject = 'Certification', course = 'Automobile Mechanics.',     id = 'CRT002' },
@@ -9,6 +9,9 @@ return {
     ['crt_06'] = { subject = 'Certification', course = 'Medical Technology.',       id = 'CRT006' },
     ['crt_07'] = { subject = 'Certification', course = 'Private Security.',         id = 'CRT007' },
 
+
+
+    ----------------------------------------------------------------
     -- special certification courses
     ['srt_11'] = { subject = 'Specialized', course = 'S.A. LEO Training #1.',       id = 'SRT011' },
     ['srt_12'] = { subject = 'Specialized', course = 'S.A. LEO Training #2.',       id = 'SRT012' },
@@ -17,40 +20,83 @@ return {
     ['srt_31'] = { subject = 'Specialized', course = 'S.A. EMS Training #1.',       id = 'SRT031' },
     ['srt_32'] = { subject = 'Specialized', course = 'S.A. EMS Training #2.',       id = 'SRT032' },
 
+
+
+    ----------------------------------------------------------------
     -- english courses
     ['eng_01'] = { subject = 'English', course = 'How to actually use grammer.',    id = 'EN112' },
     ['eng_02'] = { subject = 'English', course = 'Don\'t go "Rawr XD" in class.',   id = 'EN126' },
     ['eng_03'] = { subject = 'English', course = 'Shakespeare is overrated.',       id = 'EN135' },
     ['eng_04'] = { subject = 'English', course = 'How to form a sentence.',         id = 'EN144' },
 
+    ----------------------------------------------------------------
     -- history courses
     ['hst_01'] = { subject = 'History', course = 'Cowboys vs Aliens.',              id = 'HS110' },
     ['hst_02'] = { subject = 'History', course = 'The British did what?!',          id = 'HS123' },
     ['hst_03'] = { subject = 'History', course = 'One more score, Arthur',          id = 'HS131' },
     ['hst_04'] = { subject = 'History', course = 'How did CJ miss the train?',      id = 'HS148' },
 
+    ----------------------------------------------------------------
     -- math courses
     ['mth_01'] = { subject = 'Math', course = 'A + 20 / the Sun',                   id = 'MT113' },
     ['mth_02'] = { subject = 'Math', course = 'Why did we add letters?',            id = 'MT124' },
     ['mth_03'] = { subject = 'Math', course = 'Johnny has 3 apples',                id = 'MT137' },
     ['mth_04'] = { subject = 'Math', course = 'Pythagorean theorem > taxes',        id = 'MT142' },
 
+    ----------------------------------------------------------------
     -- science courses
     ['sci_01'] = { subject = 'Science', course = 'Photosens-deez nuts',             id = 'SC111' },
     ['sci_02'] = { subject = 'Science', course = 'Penguins + oil = bad',            id = 'SC122' },
     ['sci_03'] = { subject = 'Science', course = 'Mitochondria is the?',            id = 'SC134' },
     ['sci_04'] = { subject = 'Science', course = 'Why do we need oxygen?',          id = 'SC140' },
 
-    -- art courses
-    ['art_01'] = { subject = 'Art', course = 'What and why are lines?',             id = 'AR114' },
-    ['art_02'] = { subject = 'Art', course = 'Sniffing acrylic paints.',            id = 'AR120' },
-    ['art_03'] = { subject = 'Art', course = 'How does this make you feel?',        id = 'AR131' },
-    ['art_04'] = { subject = 'Art', course = 'An abstract arist vs a hobo.',        id = 'AR142' },
-
+    ----------------------------------------------------------------
     -- communication courses
     ['com_01'] = { subject = 'Comm.', course = 'How to talk to people.',            id = 'CM114' },
     ['com_02'] = { subject = 'Comm.', course = 'Words are hard.',                   id = 'CM126' },
     ['com_03'] = { subject = 'Comm.', course = 'Am I a bad person?',                id = 'CM138' },
-    ['com_04'] = { subject = 'Comm.', course = 'Negotiating at gunpoint.',           id = 'CM149' },
+    ['com_04'] = { subject = 'Comm.', course = 'Negotiating at gunpoint.',          id = 'CM149' },
+
+
+
+    ----------------------------------------------------------------
+    -- art degree
+    ['art_01'] = { subject = 'Art 1', course = 'What and why are lines?',           id = 'AR114' },
+    ['art_02'] = { subject = 'Art 1', course = 'Sniffing acrylic paints.',          id = 'AR120' },
+    ['art_03'] = { subject = 'Art 1', course = 'How does this make you feel?',      id = 'AR131' },
+    ['art_04'] = { subject = 'Art 1', course = 'An abstract arist vs a hobo.',      id = 'AR142' },
+    ['art_05'] = { subject = 'Art 2', course = 'Draw a perfect circle.',            id = 'AR214' },
+    ['art_06'] = { subject = 'Art 2', course = 'Sniffing more acrylic paints.',     id = 'AR220' },
+    ['art_07'] = { subject = 'Art 2', course = 'Art and Fine Art.',                 id = 'AR231' },
+    ['art_08'] = { subject = 'Art 2', course = 'Paint me a birmingham.',            id = 'AR242' },
+    ['art_09'] = { subject = 'Art 3', course = 'Oh god you\'re still here.',        id = 'AR314' },
+    ['art_10'] = { subject = 'Art 3', course = 'Animation at it\'s worst.',         id = 'AR320' },
+    ['art_11'] = { subject = 'Art 3', course = 'How to sell your art.',             id = 'AR331' },
+    ['art_12'] = { subject = 'Art 3', course = 'Why you should be a sellout.',      id = 'AR342' },
+    ['art_13'] = { subject = 'Art 4', course = 'You want a doctorate for art?',     id = 'AR414' },
+    ['art_14'] = { subject = 'Art 4', course = 'Sniffing all the acrylic paints.',  id = 'AR420' },
+    ['art_15'] = { subject = 'Art 4', course = 'What are you gonna do with this?',  id = 'AR431' },
+    ['art_16'] = { subject = 'Art 4', course = 'Everything that is art',            id = 'AR442' },
+
+
+
+    ----------------------------------------------------------------
+    -- business degree
+    ['bus_01'] = { subject = 'Bsn 1', course = 'Cons vs Business.',                 id = 'BS112' },
+    ['bus_02'] = { subject = 'Bsn 1', course = 'It\'s just business.',              id = 'BS123' },
+    ['bus_03'] = { subject = 'Bsn 1', course = 'Taking out the competition',        id = 'BS132' },
+    ['bus_04'] = { subject = 'Bsn 1', course = 'An intern vs a hobo.',              id = 'BS145' },
+    ['bus_05'] = { subject = 'Bsn 2', course = 'No don\'t say that shit.',          id = 'BS212' },
+    ['bus_06'] = { subject = 'Bsn 2', course = 'Keeping apearances.',               id = 'BS222' },
+    ['bus_07'] = { subject = 'Bsn 2', course = 'Bankruptcy? that\'s for the poor.', id = 'BS231' },
+    ['bus_08'] = { subject = 'Bsn 2', course = 'Art is for nerds.',                 id = 'BS246' },
+    ['bus_09'] = { subject = 'Bsn 3', course = 'What color suit is best?',          id = 'BS312' },
+    ['bus_10'] = { subject = 'Bsn 3', course = 'Yes, a penthouse is standard',      id = 'BS323' },
+    ['bus_11'] = { subject = 'Bsn 3', course = 'Taxes? Those are for nerds.',       id = 'BS334' },
+    ['bus_12'] = { subject = 'Bsn 3', course = 'Scapegoating the homeless.',        id = 'BS345' },
+    ['bus_13'] = { subject = 'Bsn 4', course = 'Don\'t you have a Corp. to run?',   id = 'BS416' },
+    ['bus_14'] = { subject = 'Bsn 4', course = 'Takeovers are fun if you do it.',   id = 'BS421' },
+    ['bus_15'] = { subject = 'Bsn 4', course = 'Swiss accounts are important.',     id = 'BS435' },
+    ['bus_16'] = { subject = 'Bsn 4', course = 'Why crypto is a bad idea.',         id = 'BS447' },
 
 }
