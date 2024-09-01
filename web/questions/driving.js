6193044defce74e7cb2aa2cae257dc4d2393f146
Agimir <william@mesaindigo.com>
2024-08-31 23:26:48 -0500

    working on ui

---
+++ web/questions/driving.js
@@ -0,0 +1,50 @@
+var test_driving = [
+    {
+        qs: "When you are driving into a residential area, what do you do?",
+        p1: "Slow down to meet the speed limit",
+        p2: "Maintain your speed down the road",
+        p3: "Speed up so the cops can't catch you",
+        p4: "Aim for the old people on the sidewalk",
+        pa: "A"
+    },
+    {
+        qs: "Before merging, you should:",
+        p1: "Check your blind spot",
+        p2: "Check your mirrors",
+        p3: "Set your signal before turning",
+        p4: "All of the above",
+        pa: "D"
+    },
+    {
+        qs: "question",
+        p1: "prompt",
+        p2: "prompt",
+        p3: "prompt",
+        p4: "prompt",
+        pa: "answer"
+    },
+    {
+        qs: "question",
+        p1: "prompt",
+        p2: "prompt",
+        p3: "prompt",
+        p4: "prompt",
+        pa: "answer"
+    },
+    {
+        qs: "question",
+        p1: "prompt",
+        p2: "prompt",
+        p3: "prompt",
+        p4: "prompt",
+        pa: "answer"
+    },
+    {
+        qs: "question",
+        p1: "prompt",
+        p2: "prompt",
+        p3: "prompt",
+        p4: "prompt",
+        pa: "answer"
+    },
+]
